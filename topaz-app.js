d0d7b48a2e6f2240fdec13f4ecbfed4711f09fcf
alexandrejablon <alexandre.jablon@gmail.com>
2014-02-05 10:49:19 +0100

    mock du squelette de l application

---
+++ topaz-app.js
@@ -0,0 +1,18 @@
+var mock = require('./mock.js');
+
+exports.get_index = function(req, res){
+  res.send('Topaz Server Working!');
+};
+
+exports.get_previews = function(req, res){
+  res.send(mock.get_previews(req.query.lat, req.query.long));
+};
+
+exports.get_message = function(req, res){
+  res.send(mock.get_message(req.params.id));
+}
+
+exports.post_message = function(req, res){
+  //curl -X POST -H "Content-Type:application/json" -H "Accept:application/json" http://localhost:8080/post_message -d "{lat:12,long:12,text:'Hello World'}" 
+  res.send(mock.post_message(req.body));
+}
