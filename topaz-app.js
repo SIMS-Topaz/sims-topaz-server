85f445445e221f8b3a32f0de86063fd9a642aadb
alexandrejablon <alexandre.jablon@gmail.com>
2014-02-10 10:59:47 +0100

    ajout de la liaison avec la base de données pour get_previews

--- topaz-app.js
+++ topaz-app.js
@@ -1,11 +1,21 @@
 var mock = require('./mock.js');
+var mysql_helper = require('./mysql-helper.js');
+
+mysql_helper.openConnection();
 
 exports.get_index = function(req, res){
   res.send('Topaz Server Working!');
 };
 
 exports.get_previews = function(req, res){
-  res.send(mock.get_previews(req.params.lat, req.params.long));
+  var lat = parseFloat(req.params.lat);
+  var long = parseFloat(req.params.long);var radius = 0.0015;
+  var radius = 0.0050;
+  mysql_helper.getPreviews(lat, long, radius, function (error, results) {
+    if(error)
+      console.error(error);
+    res.json (results);
+  });
 };
 
 exports.get_message = function(req, res){
