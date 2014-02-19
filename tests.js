6afb7609293acb6fd7e3d6aa13357a51bfadfe76
alexandrejablon <alexandre.jablon@gmail.com>
2014-02-19 11:23:52 +0100

    add session support and refactor the db tables

--- tests.js
+++ tests.js
@@ -1,6 +1,7 @@
 var topaz = require('./topaz-app.js');
 var mysql_helper = require('./mysql-helper.js');
 var _ = require('underscore');
+var crypto = require('crypto');
 
 exports.test_formatError = function(test){
   test.expect(1);
@@ -101,8 +102,9 @@ exports.test_prepare_post_message = function(test){
   test.done();
 };
 
-var insert_dummy = function(params, callback){
-  var query = 'INSERT INTO test_messages (`text`, `lat`, `long`, `date`) VALUES(:text, :lat, :long, :date)';
+var insert_dummy_message = function(params, callback){
+  var query = 'INSERT INTO test_messages (`text`, `lat`, `long`, `date`, `user_id`)'
+    + ' VALUES(:text, :lat, :long, :date, 99)';
   params.date = new Date().getTime();
   mysql_helper.doQuery(query, params, function(error, results){
     callback(results.insertId);
@@ -110,17 +112,11 @@ var insert_dummy = function(params, callback){
 };
 
 var insert_dummy_comment = function(params, callback){
-  var query = 'INSERT INTO test_comments (`text`, `date`, `message_id`, `user_id`) VALUES (:text, :date, :message_id, :user_id)';
+  var query = 'INSERT INTO test_comments (`text`, `date`, `message_id`, `user_id`)'
+    + ' VALUES (:text, :date, :message_id, :user_id)';
   params.date = new Date().getTime();
   mysql_helper.doQuery(query, params, function(error, results){
     callback(results.insertId);
-  });
-};
-
-var delete_dummies = function(callback){
-  var query = 'DELETE FROM test_messages';
-  mysql_helper.doQuery(query, {}, function(error, results){
-    callback();
   });
 };
 
@@ -130,13 +126,13 @@ exports.test_mysql_helper = {
     callback();
   },
   tearDown: function(callback){
-    delete_dummies(function(){
+    mysql_helper.doQuery("DELETE FROM `test_users`; DELETE FROM `test_messages`; DELETE FROM `test_comments`;", {}, function(error, results){
       mysql_helper.closeConnection();
       callback();
     });
   },
 
-  _test_environment: function(test){
+  test_environment: function(test){
     test.expect(4);
     test.equal(process.env.NODE_ENV, 'test');
     var query = 'SHOW TABLES FROM topaz';
@@ -162,7 +158,7 @@ exports.test_mysql_helper = {
 
   test_getPreviews: {
     setUp: function(callback){
-      insert_dummy({'lat': 123, 'long': 456, 'text': '221B Baker Street'}, function(){callback();});
+      insert_dummy_message({'lat': 123, 'long': 456, 'text': '221B Baker Street'}, function(){callback();});
     },
     test: function(test){
       test.expect(3);
@@ -180,7 +176,7 @@ exports.test_mysql_helper = {
 
   test_get_previews: {
     setUp: function(callback){
-      insert_dummy({'lat': 111, 'long': 101, 'text': 'Don\'t panic !'}, function(){callback();});
+      insert_dummy_message({'lat': 111, 'long': 101, 'text': 'Don\'t panic !'}, function(){callback();});
     },
     test: function(test){
       var req = {'params':{'version': 'v1.1', 'lat1': 99, 'long1': 79, 'lat2': 199, 'long2': 179}};
@@ -202,7 +198,7 @@ exports.test_mysql_helper = {
   test_postMessage: {
     test: function(test){
       test.expect(4);
-      var message = {'lat':48,'long':15,'text':'Flynn Lives !'};
+      var message = {'lat':48,'long':15,'text':'Flynn Lives !', 'user_id': 456};
       mysql_helper.postMessage(message, function(error, results){
         test.equal(error, null);
         test.notStrictEqual(results, undefined);
@@ -216,7 +212,8 @@ exports.test_mysql_helper = {
   test_post_message: {
     test: function(test){
       test.expect(4);
-      var req = {'params': {'version': 'v1.1'}, 'body': {'lat':23,'long':42,'text':'The cake is a lie'}};
+      var input = {'lat':23,'long':42,'text':'The cake is a lie'};
+      var req = {'params': {'version': 'v1.1'}, 'body': input, 'session': {'user_id': 123}};
       var res = {
         'json': function(object){
            test.equal(object.error, null);
@@ -224,9 +221,9 @@ exports.test_mysql_helper = {
            test.ok(object.data.id);
            delete object.data.date;
            delete object.data.id;
-           test.deepEqual(object, {'success': {'code': 201, 'msg': 'Created'}, 'data': {'lat': 23, 'long': 42, 'text': 'The cake is a lie'}});
-           test.done();
-        }
+	  test.deepEqual(object, {'success': {'code': 201, 'msg': 'Created'}, 'data': input});
+	  test.done();
+	}
       };
       topaz.post_message(req, res);
     }
@@ -235,7 +232,7 @@ exports.test_mysql_helper = {
   test_getMessage: {
     setUp: function(callback){
       var self = this;
-      insert_dummy({'lat': 25, 'long': 26, 'text': 'Draco Dormiens Nunquam Titillandus'}, function(id){
+      insert_dummy_message({'lat': 25, 'long': 26, 'text': 'Draco Dormiens Nunquam Titillandus'}, function(id){
         self.id = id;
         callback();
       });
@@ -258,7 +255,7 @@ exports.test_mysql_helper = {
   test_get_message: {
     setUp: function(callback){
       var self = this;
-      insert_dummy({'lat': 23, 'long': 8, 'text': 'Move fast and break things.'}, function(id){
+      insert_dummy_message({'lat': 23, 'long': 8, 'text': 'Move fast and break things.'}, function(id){
         self.id = id;
         callback();
       });
@@ -279,7 +276,7 @@ exports.test_mysql_helper = {
       topaz.get_message(req, res);
     }
   },
-  
+
   test_postComment: {
     test: function(test){
       test.expect(4);
@@ -311,5 +308,101 @@ exports.test_mysql_helper = {
 	    test.done();
       });
     }
-  }
+  },
+
+  test_get_user_auth: {
+    test_auth: function(test){
+      test.expect(2);
+      var input = {'user_name': 'sherlock', 'user_id': 99};
+      var req = {'session': input}
+      var res = {
+	json: function(object){
+	  test.notStrictEqual(object.success, undefined);
+	  test.deepEqual(object.data, input);
+	  test.done();
+	}
+      };
+      topaz.get_user_auth(req, res);
+    },
+    test_unauth: function(test){
+      test.expect(1);
+      var input = {};
+      var req = {'session': input}
+      var res = {
+	json: function(object){
+	  test.notStrictEqual(object.error, undefined);
+	  test.done();
+	}
+      };
+      topaz.get_user_auth(req, res);
+    }
+  },
+  test_post_user_auth: {
+    setUp: function(callback){
+      var shasum = crypto.createHash('sha1');
+      shasum.update('abcdef'+'watson');
+      var hash = shasum.digest('hex');
+      var self = this;
+      mysql_helper.doQuery(
+	"INSERT INTO `test_users` VALUES(null, 'john', 'john@mock.org', '"+hash+"', 'abcdef')",
+	function(error, results){
+	  self.user_id = results.insertId;
+	  callback();
+	}
+      );
+    },
+    test_auth: function(test){
+      test.expect(2);
+      var input = {'user_name': 'john', 'user_id': this.user_id};
+      var req = {'session': input}
+      var res = {
+	json: function(object){
+	  test.notStrictEqual(object.error, undefined);
+	  test.equal(object.error.code, 403);
+	  test.done();
+	}
+      };
+      topaz.post_user_auth(req, res);
+    },
+    test_unauth_no_user: function(test){
+      test.expect(2);
+      var input = {'user_name': 'mrs_hudson'};
+      var req = {'session': {}, body: input}
+      var res = {
+	json: function(object){
+	  test.notStrictEqual(object.error, undefined);
+	  test.equal(object.error.code, 401);
+	  test.done();
+	}
+      };
+      topaz.post_user_auth(req, res);
+    },
+    test_unauth_no_pwd: function(test){
+      test.expect(2);
+      var input = {'user_name': 'john', 'user_password': 'doe'};
+      var req = {'session': {}, 'body': input}
+      var res = {
+	json: function(object){
+	  test.notStrictEqual(object.error, undefined);
+	  test.equal(object.error.code, 401);
+	  test.done();
+	}
+      };
+      topaz.post_user_auth(req, res);
+    },
+    test_unauth: function(test){
+      test.expect(2);
+      var input = {'user_name': 'john', 'user_password': 'watson'};
+      var req = {'session': {}, 'body': input}
+      var res = {
+	json: function(object){
+	  test.notStrictEqual(object.success, undefined);
+	  test.equal(object.data.user_name, 'john');
+	  test.done();
+	}
+      };
+      topaz.post_user_auth(req, res);
+    }
+  },
 };
+
--- topaz.sql
+++ topaz.sql
@@ -2,7 +2,7 @@ CREATE SCHEMA `topaz`;
 
 CREATE TABLE `topaz`.`messages` (
   `id` BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  `username` TEXT NOT NULL,
+  `user_id` TEXT NOT NULL,
   `text` TEXT NOT NULL,
   `lat` FLOAT NOT NULL,
   `long` FLOAT NOT NULL,
@@ -10,30 +10,32 @@ CREATE TABLE `topaz`.`messages` (
   `likes` INT UNSIGNED NULL DEFAULT 0,
   `dislikes` INT UNSIGNED NULL DEFAULT 0
 );
+CREATE TABLE `topaz`.`test_messages` LIKE `topaz`.`messages`;
 
 CREATE TABLE `topaz`.`comments` (
   `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `text` TEXT NOT NULL,
   `date` BIGINT NOT NULL,
-  `id_message` BIGINT UNSIGNED NOT NULL,
-  `username` TEXT NOT NULL,
+  `message_id` BIGINT UNSIGNED NOT NULL,
+  `user_id` TEXT NOT NULL,
   `likes` INT UNSIGNED NULL DEFAULT 0,
   `dislikes` INT UNSIGNED NULL DEFAULT 0
 );
+CREATE TABLE `topaz`.`test_comments` LIKE `topaz`.`comments`;
 
 CREATE TABLE `topaz`.`users` (
   `id` BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  `username` TEXT NOT NULL,
+  `name` TEXT NOT NULL,
   `email` TEXT NOT NULL,
   `password`  VARCHAR(40),
-  `salt`  VARCHAR(40),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC));
+  `salt`  VARCHAR(40)
 );
+CREATE TABLE `topaz`.`test_users` LIKE `topaz`.`users`;
 
 CREATE TABLE `topaz`.`votes` (
-  `user` VARCHAR(20) NOT NULL,
-  `id_comment` BIGINT NOT NULL,
+  `user_id` VARCHAR(20) NOT NULL,
+  `comment_id` BIGINT NOT NULL,
   `vote` VARCHAR(4) NOT NULL,
-  PRIMARY KEY (`id_user`, `id_comment`)
+  PRIMARY KEY (`user_id`, `comment_id`)
 );
+CREATE TABLE `topaz`.`test_votes` LIKE `topaz`.`votes`;
